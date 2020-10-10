f75211d912ca0f18adc4a4f0f8ca8a88969a65bf
liujin1992112 <scliujin1992112@163.com>
2020-10-10 19:23:33 +0800

    完善xlsx转lua字符串

--- XlsxToLua/node/src/xlsxtolua/TableExportToLuaHelper.js
+++ XlsxToLua/node/src/xlsxtolua/TableExportToLuaHelper.js
@@ -63,7 +63,7 @@ TableExportToLuaHelper.exportTableToLua = function(tableInfo) {
 
         }
         content += `[${row}]`;
-        content += "={";
+        content += " = {";
         currentLevel++;
         for(let column = 0; column < columnCount; column++) {
             content += TableExportToLuaHelper._getOneField(allField[column], row, currentLevel);
@@ -79,13 +79,84 @@ TableExportToLuaHelper.exportTableToLua = function(tableInfo) {
 
 TableExportToLuaHelper._getOneField = function(fieldInfo, row, level) {
     let content = "";
+
+    // 变量名前的缩进
+    content += TableExportToLuaHelper.getLuaTableIndentation();
+
+    // 变量名
     content += fieldInfo.fieldName;
-    content += "=";
-    content +=  fieldInfo.data[row];
+    content += " = ";
+
+    let value = null;
+    switch(fieldInfo.dataType) {
+        case DataType.Int:
+        case DataType.Long:
+        case DataType.Float:
+            value = TableExportToLuaHelper._GetNumberValue(fieldInfo, row, level)
+            break;
+        case DataType.String:
+            value = TableExportToLuaHelper._GetStringValue(fieldInfo, row, level);
+            break;
+        case DataType.Bool:
+            value = TableExportToLuaHelper._GetBoolValue(fieldInfo, row, level);
+            break;
+        case DataType.Lang:
+            value = TableExportToLuaHelper._GetLangValue(fieldInfo, row, level);
+            break;
+        case DataType.Date:
+            value = TableExportToLuaHelper._GetDateValue(fieldInfo, row, level);
+            break;
+        case DataType.Time:
+            value = TableExportToLuaHelper._GetTimeValue(fieldInfo, row, level);
+            break;
+        case DataType.Json:
+            value = TableExportToLuaHelper._GetJsonValue(fieldInfo, row, level);
+            break;
+        case DataType.TableString:
+            value = TableExportToLuaHelper._GetTableStringValue(fieldInfo, row, level);
+            break;
+        case DataType.MapString:
+            value = TableExportToLuaHelper._GetMapStringValue(fieldInfo, row, level);
+            break;
+        case DataType.Dict:
+        case DataType.Array:
+            value = TableExportToLuaHelper._GetSetValue(fieldInfo, row, level);
+            break;
+    }
+
+    content +=  value;
     content += ","
     return content;
 }
 
+TableExportToLuaHelper._GetNumberValue = function(fieldInfo, row, level) {
+    if (fieldInfo.data[row] == null)
+        return "nil";
+    else
+        return fieldInfo.data[row].ToString();
+}
+
+TableExportToLuaHelper._GetStringValue = function(fieldInfo, row, level) {
+    // 将单元格中填写的英文引号进行转义，使得单元格中填写123"456时，最终生成的lua文件中为xx = "123\"456"
+    // 将单元格中手工按下的回车变成"\n"输出到lua文件中，单元格中输入的"\n"等原样导出到lua文件中使其能被lua转义处理。之前做法为Replace("\\", "\\\\")，即将单元格中输入内容均视为普通字符，忽略转义的处理
+    let str = new String(fieldInfo.data[row]);
+    str.replace("\n", "\\n").replace("\"", "\\\"");
+    
+    let content = "";
+    content += "\"";
+    content += str;
+    content += "\"";
+
+    return content;
+}
+
+TableExportToLuaHelper._GetBoolValue = function(fieldInfo, row, level)
+{
+    if (fieldInfo.data[row] == true)
+        return "true";
+    else
+        return "false";
+}
 
 TableExportToLuaHelper.prototype = {
     constructor: TableExportToLuaHelper,
