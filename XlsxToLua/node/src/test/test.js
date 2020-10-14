f4c7bd47c942898f29a227d82a0f451ce737c80b
liujin1992112 <scliujin1992112@163.com>
2020-10-14 19:02:36 +0800

    json解析异常处理

--- XlsxToLua/node/src/test/test.js
+++ XlsxToLua/node/src/test/test.js
@@ -1,10 +1,33 @@
-//测试js类型转换
-var convertInt = parseInt("a")
-console.log(convertInt)
+function testParseInt() {
+    //测试js类型转换
+    var convertInt = parseInt("a")
+    console.log(convertInt)
 
-//NaN判断
-var isRight = isNaN(convertInt)
-if(isRight) {
-    throw new Error("类型转换异常")
+    //NaN判断
+    var isRight = isNaN(convertInt)
+    if(isRight) {
+        throw new Error("类型转换异常")
+    }
+    console.log(isRight)
 }
-console.log(isRight)
+// testParseInt()
+
+function testParseJson() {
+    var obj = {}
+    obj.name = "liujin"
+    obj.age = 18
+
+    var jsonStr = JSON.stringify(obj)
+    console.log(jsonStr)
+
+    try {
+        var jsonStr = jsonStr.substring(0, jsonStr.length - 2)
+        console.log(JSON.parse(jsonStr))
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+testParseJson()
+
